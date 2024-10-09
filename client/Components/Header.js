57969408fd9a121f98436d811d4d44b338a9b737
James O'Reilly <James.Oreilly926@gmail.com>
2024-10-09 13:46:17 -0600

    add logo to HomePage component

--- client/Components/Header.js
+++ client/Components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Logo = styled.img`
+  width: 90px;
+  height: 90px;
+`
 
 const Header = () => (
   <div>
-    Hello from Header!
+    <Logo src="/images/logo.png" />
   </div>
 )
 
