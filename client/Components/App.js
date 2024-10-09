d1860da712f16fb6bcddb187c5fc99ea1401779e
James O'Reilly <James.Oreilly926@gmail.com>
2024-10-09 13:00:08 -0600

    create and add HomePage.js to our App.js file at front end path /

--- client/Components/App.js
+++ client/Components/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Header, Footer } from '../Components'
+import { Header, Footer, HomePage } from '../Components'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
       <Footer />
     </Router>
   )
---
+++ client/Components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+
+`
+
+const Homepage = () => (
+  <Wrapper>
+    Hello from HomePage
+  </Wrapper>
+)
+
+export default Homepage
