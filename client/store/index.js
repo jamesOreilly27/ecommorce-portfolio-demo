b7eef43339b7cb607c67028f6d218747c74374d8
James O'Reilly <James.Oreilly926@gmail.com>
2024-10-10 12:22:49 -0600

    add middleware at client/store/index.js

--- client/store/index.js
+++ client/store/index.js
@@ -1,18 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { productApi } from "./productSlice"
+import logger from 'redux-logger'
+import { composeWithDevTools } from '@redux-devtools/extension'
+
+const errHandling = store => next => action => {
+  try {
+    return next(action)
+  } catch (error) {
+    console.error(`Error dispatching action: ${action.type} with payload: ${JSON.stringify(action.payload)}`, error)
+  }
+}
 
 const reducer = {
   [productApi.reducerPath]: productApi.reducer
 }
 
-const apiMiddlewares = [
-  productApi.middleware
+const middleware = [
+  productApi.middleware,
+  logger,
+  composeWithDevTools,
+  errHandling
 ]
 
 const store = configureStore({
   reducer: reducer,
   middleware:  getDefaultMiddleware =>
-    getDefaultMiddleware().concat(...apiMiddlewares)
+    getDefaultMiddleware().concat(...middleware)
 })
 
 export default store
