515abce9c4cca9b25b56add61c6698a0b7f5dba9
James O'Reilly <James.Oreilly926@gmail.com>
2024-10-10 20:41:51 -0600

    add style and message to Categories component

--- client/Components/Categories.js
+++ client/Components/Categories.js
@@ -2,12 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-
+  background-color: red;
+  width: 100vw;
+  height: 50vh;
 `
 
 const Categories = () => (
   <Wrapper>
-
+    This is a container for categories
   </Wrapper>
 )
 
