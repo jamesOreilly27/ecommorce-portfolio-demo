64dabdddd406547231a7056468c083f60337408c
James O'Reilly <James.Oreilly926@gmail.com>
2024-10-10 20:35:42 -0600

    make skeleton for CategoryCard component

---
+++ client/Components/CategoryCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+
+`
+
+const CategoryCard = ({ category }) => (
+  <Wrapper>
+    This is a category card
+  </Wrapper>
+)
+
+export default CategoryCard
