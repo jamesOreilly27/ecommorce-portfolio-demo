2052c93d63e11fe1f90ca5a725cca297eb707669
James O'Reilly <James.Oreilly926@gmail.com>
2024-10-12 00:31:27 -0600

    create api route for five star reviews

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const chalk = require('chalk')
 const { Review } = require('../models')
-const { findById, getAll } = require('./helpers')
+const { findById, getAll, getAllWhere } = require('./helpers')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
   getAll(res, Review)
+})
+
+router.get('/five-star', (req, res, next) => {
+  getAllWhere(res, Review, { rating: 5 })
 })
 
 module.exports = router
