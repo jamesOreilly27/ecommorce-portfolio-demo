7858ee4227248aaf2c6d1dc4134ae32ec5d0ac96
James O'Reilly <James.Oreilly926@gmail.com>
2024-10-12 00:11:09 -0600

    create api route for featured categories

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const { Category, Product } = require('../models')
-const { findById, getAll } = require('./helpers')
+const { findById, getAll, getAllWhere } = require('./helpers')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
   getAll(res, Category)
+})
+
+router.get('/featured', (req, res, next) => {
+  getAllWhere(res, Category, { featured: true }, [Product])
 })
 
 router.get('/:id', (req, res, next) => {
