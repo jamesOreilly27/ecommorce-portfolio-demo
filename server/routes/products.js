c782a8629003e31b59e6fadec96f0f61310993d3
James O'Reilly <James.Oreilly926@gmail.com>
2024-10-17 17:06:24 -0600

    update the all products route to include logic for categoryIds in req.query

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,34 +1,50 @@
 const express = require('express')
+const { Op } = require('sequelize');
 const chalk = require('chalk')
-const { Product, Category, Review } = require('../models')
+const { Product, Category, ProductCategory, Review } = require('../models')
 const { findById, getAll, getAllWhere } = require('./helpers')
 
 const router = express.Router()
 
 /***** All Products *****/
 router.get('/', (req, res, next) => {
-  getAll(res, Product, [Category, Review])
+  const { categoryIds } = req.query
+  const parsedCategoryIds = categoryIds ? categoryIds.split(',').map(Number) : []
+
+  const association = { model: Category }
+  if(categoryIds) {
+    association['where'] = { id: { [Op.in]: parsedCategoryIds } }
+  }
+
+  Product.findAll({ include: association })
+  .then(response => res.json(response))
+  .catch(err => res.json(err))
 })
 
+
+/***** Featured Products *****/
 router.get('/featured', (req, res, next) => {
   getAllWhere(res, Product, { featured: true })
 })
+
+// router.get('/filtered', (req, res, next) => {
+//   const { categoryIds } = req.query
+//   const parsedCategoryIds = categoryIds.split(',').map(Number)
+
+//   const association = { model: Category }
+//   if(categoryIds) {
+//     association['where'] = { id: { [Op.in]: parsedCategoryIds } }
+//   }
+
+//   Product.findAll({ include: association })
+//   .then(response => res.json(response))
+//   .catch(err => res.json(err))
+// })
 
 
 /***** Get by ID *****/
 router.get('/:id', (req, res, next) => {
   findById(req, res, Product, [Review, Category])
-})
-
-router.get('/filtered', (req, res, next) => {
-  const { categoryIds } = req.query
-  Product.findAll({
-    where: {
-      [or]: [
-        { categoryId: []}
-      ]
-    }
-  })
 })
 
 module.exports = router
